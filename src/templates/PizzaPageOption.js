f7c768a70253dfd307e1cf6cece4901d7bbf2397
maximedinaux <maxime@dinaux.be>
2020-11-26 00:25:01 +0100

    Update PizzaPageOption.js

--- src/templates/PizzaPageOption.js
+++ src/templates/PizzaPageOption.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { graphql } from "gatsby"
 import BackgroundImage from "gatsby-background-image"
 
@@ -6,6 +6,17 @@ import BackgroundImage from "gatsby-background-image"
 //css
 import "../css/pizza-page-option.scss"
 const PizzaPage = ({ data }) => {
+  const choiceTaille = [{ "1": data.c.prixRegular }, { "2": data.c.prixMaxi }]
+  const [taille, setTaille] = useState(0)
+  console.log(choiceTaille[taille - 1])
+
+  const [order, setOrder] = useState("")
+
+  const reset = () => {
+    setOrder(order => "")
+    setTaille(taille => 0)
+  }
+
   return (
     <div className={`container pizzaOption `}>
       <div className="fullWidth return">Return</div>
@@ -15,45 +26,94 @@ const PizzaPage = ({ data }) => {
           <div className="contentPizza">
             <h1>{data.c.title}</h1>
             <p>{data.c.description.description}</p>
-            <h2>Taille</h2>
+            <h3>Supplément sélectionné</h3>
+            {order ? (
+              <div>
+                {order.map((item, index) => {
+                  return (
+                    <div key={index}>
+                      {item.name}#{item.price}
+                    </div>
+                  )
+                })}
+              </div>
+            ) : (
+              <div>Aucun</div>
+            )}
+            <h2>Taille </h2>
             <div className="taille">
-              <button>
+              <button
+                onClick={() => setTaille(taille => 1)}
+                disabled={taille ? true : false}
+              >
                 Regular<span>{data.c.prixRegular} €</span>
               </button>
-              <button>
+              <button
+                onClick={() => setTaille(taille => 2)}
+                disabled={taille ? true : false}
+              >
                 Maxi<span>{data.c.prixMaxi} €</span>
               </button>
             </div>
-            <h2>Supplément</h2>
-            <h3>Légumes</h3>
-            {data.d.nodes.map((item, index) => {
-              return (
-                <button key={index}>
-                  {item.title}
-                  <span>{item.priceRegular}</span>
-                </button>
-              )
-            })}
-            <h3>Viandes</h3>
-            {data.e.nodes.map((item, index) => {
-              return (
-                <button key={index}>
-                  {item.title}
-                  <span>{item.priceRegular}</span>
-                </button>
-              )
-            })}
-            <h3>Poisson</h3>
-            {data.f.nodes.map((item, index) => {
-              return (
-                <button key={index}>
-                  {item.title}
-                  <span>{item.priceRegular}</span>
-                </button>
-              )
-            })}
+            {taille !== 0 ? (
+              <>
+                <h2>Supplément</h2>
+                <div className="supp">
+                  {data.d.nodes.map((item, index) => {
+                    return (
+                      <div key={index}>
+                        <h3>
+                          {item.title}
+                          <span>
+                            {taille === 1
+                              ? item.prixRegular + "€/p"
+                              : taille === 2
+                              ? item.prixMaxi + "€/p"
+                              : ""}
+                          </span>
+                          <div className="suppList">
+                            {item.supplement.map((item, index) => {
+                              return (
+                                <button
+                                  key={index}
+                                  onClick={
+                                    taille === 1
+                                      ? () =>
+                                          setOrder(order => [
+                                            ...order,
+                                            {
+                                              name: item.title,
+                                              price: item.type.prixRegular,
+                                            },
+                                          ])
+                                      : () =>
+                                          setOrder(order => [
+                                            ...order,
+                                            {
+                                              name: item.title,
+                                              price: item.type.prixMaxi,
+                                            },
+                                          ])
+                                  }
+                                >
+                                  {item.title}
+                                </button>
+                              )
+                            })}
+                          </div>
+                        </h3>
+                      </div>
+                    )
+                  })}
+                </div>
+              </>
+            ) : (
+              ""
+            )}
+
             <div className="action">
               <button>Ajouter au panier</button>
+              <button onClick={() => reset()}>Réinitialiser</button>
             </div>
           </div>
         </div>
@@ -78,34 +138,20 @@ export const query = graphql`
         }
       }
     }
-    d: allContentfulSupplement(
-      filter: { type: { eq: "légumes" } }
-      sort: { fields: title, order: ASC }
+    d: allContentfulSupplementPrix(
+      sort: { fields: [order, supplement___title], order: ASC }
     ) {
       nodes {
         title
-        prixRegular
         prixMaxi
-      }
-    }
-    e: allContentfulSupplement(
-      filter: { type: { eq: "viandes" } }
-      sort: { fields: title, order: ASC }
-    ) {
-      nodes {
-        title
         prixRegular
-        prixMaxi
-      }
-    }
-    f: allContentfulSupplement(
-      filter: { type: { eq: "poissons" } }
-      sort: { fields: title, order: ASC }
-    ) {
-      nodes {
-        title
-        prixRegular
-        prixMaxi
+        supplement {
+          title
+          type {
+            prixMaxi
+            prixRegular
+          }
+        }
       }
     }
   }
